cb6e5bee0c4556d05dba17ca69572900bbc21c3f
Per Lundberg <per@halleluja.nu>
2008-11-16 20:42:09 +0000

    Fixed a typo

--- Kernel/32BitLoader/64bit.h
+++ Kernel/32BitLoader/64bit.h
@@ -6,8 +6,8 @@
  * Author: Per Lundberg <per@halleluja.nu> 
  */
 
-#ifndef __INT64_H__
-#define __INT64_H__ 1
+#ifndef __64BIT_H__
+#define __64BIT_H__ 1
 
 /* This is to make sure that the assembly files can still include multiboot.h without getting compilation errors. */
 #ifndef __ASSEMBLER__
@@ -17,4 +17,4 @@ extern void _64bit_init(void *multiboot_header, uint64_t highest_address);
 // The entry point of the 64-bit kernel.
 #define _64BIT_KERNEL_ENTRY_POINT        0x100000
 
-#endif /* ! __INT64_H__ */
+#endif // ! __64bIT_H__
