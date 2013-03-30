a93b59e208bf6bb5d1b9fef2fff0f585a5902008
Per Lundberg <perlun@gmail.com>
2013-03-31 00:52:57 +0200

    Completely silly, but it feels even more silly to install a 32-bit crosscompiler for the very small bits that are actually 32-bit in this kernel.

---
+++ Kernel/gnu/stubs-32.h
@@ -0,0 +1,7 @@
+/*
+ * stubs-32.h - Dummy file needed since my gcc 4.7.2 cross-compiler on OSX expects this file to exist (I only have a 64-bit cross
+ * compiler, so that's probably why it's needed).
+ *
+ * Author: Per Lundberg <per@halleluja.nu>
+ * Copyright: © 2013 Per Lundberg
+ */
